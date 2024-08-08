463951c47b29c6b56fedd70da85a26f2bcc2e1d7
ashokdevtron <ashok.nayak@devtron.ai>
2024-08-08 20:23:58 +0530

    Deploy website - based on cb9e6e492de23b27b3adaf45b09560711a574496

---
+++ assets/js/a7456010.fc56e6fe.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknewdoc=self.webpackChunknewdoc||[]).push([[235],{8552:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
---
+++ assets/js/acecf23e.e77ecb3f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknewdoc=self.webpackChunknewdoc||[]).push([[903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/newdoc/blog","blogTitle":"Blog"}')}}]);
